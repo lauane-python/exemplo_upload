fcf1884d9b8d00935980a22dfc109d3323a5c122
Lauane Pasquini <lauanepasquine8@gmail.com>
2026-02-26 08:41:50 -0300

    ok

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const multer = require('multer')
+
+const cors = require('cors')
+
+const server = express()
+
+server.listen(3000, () => console.log("rodando em  http://localhost:3000"))
